0c3ebadb7dddd91b73ec5d6b790af5b3bb5528d7
Ryan O'Shaughnessy <ryan.oshag@gmail.com>
2013-02-21 20:34:18 -0500

    Added crab steering capability; set up basic crab steering
    Switch to crab steering with "Enter"; crab steering at the moment has
    all wheels move together and rover moves in direction of wheels.  Not
    sure how to control individual wheels moving on their own.

--- RoverPanel.java
+++ RoverPanel.java
@@ -15,8 +15,10 @@ public class RoverPanel extends JPanel implements ActionListener, KeyListener
    private final int IMAGE_SIZE = 35;
    private ImageIcon image;
    private JButton faster,slower,pause;
+	private JButton modeButton;
 	private Timer timer;
    private double x, y, moveX, moveY;
+	private boolean mode;		//mode = false -> Ackermann, mode = true -> Crab
    //-----------------------------------------------------------------
    //  Sets up the panel, including the timer for the animation.
    //-----------------------------------------------------------------
@@ -28,6 +30,8 @@ public class RoverPanel extends JPanel implements ActionListener, KeyListener
       y = 200;
       moveX = 0;
       moveY =0;
+		mode = false;
+		setFocusable(false);
       addKeyListener(this);
       setFocusable(true);
       setFocusTraversalKeysEnabled(false);
@@ -48,93 +52,145 @@ public class RoverPanel extends JPanel implements ActionListener, KeyListener
         super.paintComponent (page);
         Graphics2D g2d = (Graphics2D) page;
 		  
-		  g2d.setColor(Color.BLACK);
-        Rectangle2D.Double frontLeftWheel = new Rectangle2D.Double(-35+x,-40+y, 10, 25);
-        g2d.translate(x, y);
-        g2d.rotate(Math.toRadians(moveX));
-        g2d.translate(-x, -y);
-        g2d.fill(frontLeftWheel);
-        
-        g2d.setColor(Color.BLACK);
-        Rectangle2D.Double frontRightWheel = new Rectangle2D.Double(25+x,-40+y, 10, 25);
-        g2d.fill(frontRightWheel);
-        
-		  g2d.setColor(Color.GRAY);
-        Rectangle2D.Double body = new Rectangle2D.Double(-25+x,-50+y, 50, 100);
-		  g2d.translate(x, y);
-		  g2d.rotate(Math.toRadians(-moveX));
-		  g2d.rotate(Math.toRadians(moveX/2));
-		  g2d.translate(-x,-y);
-        g2d.fill(body);
+		  if(mode) {
+		     g2d.setColor(Color.GRAY);
+	        Rectangle2D.Double body = new Rectangle2D.Double(-25+x,-50+y, 50, 100);
+	        g2d.fill(body);
 		  
-		  g2d.setColor(Color.BLACK);
-        Rectangle2D.Double backRightWheel = new Rectangle2D.Double(25+x, 20+y, 10, 25);
-        g2d.fill(backRightWheel);
+		  	  g2d.setColor(Color.BLACK);
+	        Rectangle2D.Double frontLeftWheel = new Rectangle2D.Double(-35+x,-40+y, 10, 25);
+	        g2d.translate(-35+5+x, -40+12.5+y);	//translate coordinates to center of wheel for rotation
+	        g2d.rotate(Math.toRadians(moveX));
+	        g2d.translate(35-5-x, 40-12.5-y);
+	        g2d.fill(frontLeftWheel);
+			  g2d.translate(-35+5+x, -40+12.5+y);	//translate coordinates back to origin
+	        g2d.rotate(Math.toRadians(-moveX));
+	        g2d.translate(35-5-x, 40-12.5-y);
+	        
+	        g2d.setColor(Color.BLACK);
+	        Rectangle2D.Double frontRightWheel = new Rectangle2D.Double(25+x,-40+y, 10, 25);
+			  g2d.translate(25+5+x, -40+12.5+y);	//translate coordinates to center of wheel for rotation
+	        g2d.rotate(Math.toRadians(moveX));
+	        g2d.translate(-25-5-x, 40-12.5-y);
+	        g2d.fill(frontRightWheel);
+			  g2d.translate(25+5+x, -40+12.5+y);	//translate coordinates back to origin
+	        g2d.rotate(Math.toRadians(-moveX));
+	        g2d.translate(-25-5-x, 40-12.5-y);
 		  
-		  g2d.setColor(Color.BLACK);
-        Rectangle2D.Double backLeftWheel = new Rectangle2D.Double(-35+x, 20+y, 10, 25);
-        g2d.fill(backLeftWheel);
-        
-        
-        
-        
-   }
+			  g2d.setColor(Color.BLACK);
+	        Rectangle2D.Double backRightWheel = new Rectangle2D.Double(25+x, 20+y, 10, 25);
+	        g2d.translate(25+5+x, 20+12.5+y);	//translate coordinates to center of wheel for rotation
+	        g2d.rotate(Math.toRadians(moveX));
+	        g2d.translate(-25-5-x, -20-12.5-y);
+	        g2d.fill(backRightWheel);
+			  g2d.translate(25+5+x, 20+12.5+y);	//translate coordinates back to origin
+	        g2d.rotate(Math.toRadians(-moveX));
+	        g2d.translate(-25-5-x, -20-12.5-y);
+			  
+			  g2d.setColor(Color.BLACK);
+	        Rectangle2D.Double backLeftWheel = new Rectangle2D.Double(-35+x, 20+y, 10, 25);
+	        g2d.translate(-35+5+x, 20+12.5+y);	//translate coordinates to center of wheel for rotation
+	        g2d.rotate(Math.toRadians(moveX));
+	        g2d.translate(35-5-x, -20-12.5-y);
+	        g2d.fill(backLeftWheel);
+			  g2d.translate(-35+5+x, 20+12.5+y);	//translate coordinates back to origin
+	        g2d.rotate(Math.toRadians(-moveX));
+	        g2d.translate(35-5-x, -20-12.5-y);
+        }	//end if (Crab) 
+		  
+		  else {
+			  g2d.setColor(Color.BLACK);
+	        Rectangle2D.Double frontLeftWheel = new Rectangle2D.Double(-35+x,-40+y, 10, 25);
+	        g2d.translate(x, y);
+	        g2d.rotate(Math.toRadians(moveX));
+	        g2d.translate(-x, -y);
+	        g2d.fill(frontLeftWheel);
+	        
+	        g2d.setColor(Color.BLACK);
+	        Rectangle2D.Double frontRightWheel = new Rectangle2D.Double(25+x,-40+y, 10, 25);
+	        g2d.fill(frontRightWheel);
+	        
+			  g2d.setColor(Color.GRAY);
+	        Rectangle2D.Double body = new Rectangle2D.Double(-25+x,-50+y, 50, 100);
+			  g2d.translate(x, y);
+			  g2d.rotate(Math.toRadians(-moveX));
+			  g2d.rotate(Math.toRadians(moveX/2));
+			  g2d.translate(-x,-y);
+	        g2d.fill(body);
+		  
+			  g2d.setColor(Color.BLACK);
+	        Rectangle2D.Double backRightWheel = new Rectangle2D.Double(25+x, 20+y, 10, 25);
+	        g2d.fill(backRightWheel);
+			  
+			  g2d.setColor(Color.BLACK);
+	        Rectangle2D.Double backLeftWheel = new Rectangle2D.Double(-35+x, 20+y, 10, 25);
+	        g2d.fill(backLeftWheel);
+		  }	//end else (Ackermann)
+		  
+          
+   }	//end paintComponent
+	
 	/** this method changes delay of the repaint method thought timer.
 	* @param delta determines what the change in the timer will be on click
 	*/
-   public void actionPerformed(ActionEvent e){
-
+   public void actionPerformed(ActionEvent e) {
 	   repaint();
        x-=moveY*Math.sin(Math.toRadians(moveX));
        y+=moveY*Math.cos(Math.toRadians(moveX));
-       
-   }
+   }	//end actionPerformed
    public void up(){
        moveY-=.5;
 		 //moveX*=1.1;
-   }
+   }	//end up
    public void down(){
        moveY+=.5;
 		 //moveX*=1.1;
-   }
+   }	//end down
    public void left(){
        moveX-=2;
-   }
+   }	//end left
    public void right(){
        moveX+=2;
-   }
+   }	//end right
    public void keyPressed(KeyEvent e){
        int code = e.getKeyCode();
        if(code==KeyEvent.VK_UP){
            up();
-       }
+       }	//end if
        if(code==KeyEvent.VK_DOWN){
            down();
-       }
+       }	//end if
        if(code==KeyEvent.VK_LEFT){
            left();
-       }
+       }	//end if
        if(code==KeyEvent.VK_RIGHT){
            right();
-       }  
-   }
+       } //end if 
+		 if(code==KeyEvent.VK_ENTER){
+		 	if(mode == false)	//switch steering mode on key input of enter
+				mode = true;
+			else
+				mode = false;
+		 }	//end if
+   }	//end keyPressed
    public void keyTyped(KeyEvent e){}
    public void keyReleased(KeyEvent e){
 	   int code = e.getKeyCode();
        if(code==KeyEvent.VK_UP){
-           moveX = moveX*Math.pow(1.1,(moveY/.5));
+           //moveX = moveX*Math.pow(1.1,(moveY/.5));
 			  moveY=0;
-       }
+       }	//end if
        if(code==KeyEvent.VK_DOWN){
-		 	  moveX = moveX*Math.pow(1.1,(moveY/.5));
+		 	  //moveX = moveX*Math.pow(1.1,(moveY/.5));
            moveY=0;
-       }
+       }	//end if
        if(code==KeyEvent.VK_LEFT){
         //   moveX=0;
-       }
+       }	//end if
        if(code==KeyEvent.VK_RIGHT){
          //  moveX=0;
-       }
-   }
+       }	//end if
+   }	//end keyReleased
+	
 }
 	
